77a8b6db96ae76a645bdf97b7b0d05d7ca1fb70f
sathlan <chem@sathlan.org>
2013-02-27 09:59:13 +0100

    Put part of the network configuration after boot and before live network.

--- lib/vagrant/host_gateway/enhance.rb
+++ lib/vagrant/host_gateway/enhance.rb
@@ -15,6 +15,21 @@ module Vagrant
         Vagrant::HostGateway::Host.new(env).enhance!
         Vagrant::HostGateway::Guest.new(env).enhance!
 
+        env[:vm].config.vm.networks.each do |type, args|
+          if type == :hostonly
+            ip = args[0]
+            options = { :nat => false }.merge(args[1] || {})
+            if nic = options[:nat]
+              env[:ui].info "Enabling forwarding on host."
+              env[:host].enable_forwarding
+              traffic = "#{ip}/#{Middleware.network_to_cidr(options[:netmask])}"
+              env[:ui].info "Setting up SNAT on #{nic} catching traffic from #{traffic}"
+              env[:host].setup_nat(nic, traffic)
+            end
+            # record if the user want to setup the nic if they are already set up
+            env[:vm].guest.want_create_only_for(ip => true) if options[:create_only]
+          end
+        end
       end
     end
   end
--- lib/vagrant/host_gateway/middleware.rb
+++ lib/vagrant/host_gateway/middleware.rb
@@ -7,8 +7,6 @@ module Vagrant
       end
 
       def call(env)
-        Vagrant::HostGateway::Guest.new(@env).enhance!
-        Vagrant::HostGateway::Host.new(@env).enhance!
 
         @app.call(env)
 
@@ -19,19 +17,6 @@ module Vagrant
           @env[:vm].guest.set_gateway(gateway)
         end
 
-        @env[:vm].config.vm.networks.each do |type, args|
-          if type == :hostonly
-            ip = args[0]
-            options = { :nat => false }.merge(args[1] || {})
-            if nic = options[:nat]
-              @env[:ui].debug "Enabling forwarding"
-              @env[:host].enable_forwarding
-              traffic = "#{ip}/#{network_to_cidr(options[:netmask])}"
-              @env[:ui].debug "Setting up SNAT on #{nic} catching traffic from #{traffic}"
-              @env[:host].setup_nat(nic, traffic)
-            end
-          end
-        end
       end
 
       def network_to_cidr(network)
