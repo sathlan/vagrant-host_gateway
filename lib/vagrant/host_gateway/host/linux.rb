5e3ab056add9d1d4a4aa47e9de3148ef7cb672e0
sathlan <chem@sathlan.org>
2013-02-27 10:03:43 +0100

    Improve linux host command.

--- lib/vagrant/host_gateway/host/linux.rb
+++ lib/vagrant/host_gateway/host/linux.rb
@@ -11,7 +11,7 @@ module Vagrant
             raise InvalidInterface
           end
           unless system(%Q/sudo iptables -t nat -L POSTROUTING -nvx | grep 'MASQUERADE.*#{nic}.*#{net}'/)
-            system("sudo iptables -t nat -I POSTROUTING 1 -o #{nic} -s #{net} -j MASQUERADE")
+            system("sudo iptables -t nat -I POSTROUTING 1 -o #{nic} -s #{net} -j MASQUERADE -m comment --comment 'Done by Vagrant'")
           end
         end
       end
